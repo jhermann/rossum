4d9aff83d9a05f4853193bc88724d0c80364f0ba
jhermann <jh@web.de>
2013-09-10 19:29:03 +0200

    Added some comments

--- apply.sh
+++ apply.sh
@@ -9,7 +9,7 @@ self="$base/"$(basename "$0")
 abspath() {
     echo $(cd $(dirname "$1") && pwd)/$(basename "$1")
 }
-apply() {
+apply() { # Apply manifests located in the same directory as this script
     puppet apply --node_terminus exec --external_nodes "$self" \
         --modulepath $base/modules $base/manifests/site.pp
 }
@@ -19,19 +19,23 @@ action="$1"; test -z "$action" || shift
 
 case "$action" in
     enc | "$(hostname -f)")
+        # Callback from Puppet, see "--external_nodes" above
         cat ${ROSSUM_ENC:-/etc/puppet/rossum.yaml}
         ;;
     -* | -h | --help | help)
         echo "usage:" $(basename "$0") "[apply <enc.yaml>]"
         ;;
     apply)
+        # Apply manifests, take optional ENC argument
         test -z "$1" || export ROSSUM_ENC=$(abspath "$1")
         apply
         ;;
     "")
+        # Apply manifests with default ENC (or one already set in ROSSUM_ENC)
         apply
         ;;
     *)
+        # Interpret any odd argument as an ENC filename
         export ROSSUM_ENC=$(abspath "$action")
         apply
         ;;
